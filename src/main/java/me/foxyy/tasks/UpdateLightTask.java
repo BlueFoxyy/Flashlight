29c4b8459d93a9f893f150b2140b698ceaaefd16
Foxyy <ryanchang0223@gmail.com>
2025-01-13 21:26:27 +0800

    Fixed a bug where you cannot light fire to blocks with flashlight on.

--- src/main/java/me/foxyy/tasks/UpdateLightTask.java
+++ src/main/java/me/foxyy/tasks/UpdateLightTask.java
@@ -1,6 +1,7 @@
 package me.foxyy.tasks;
 
 import me.foxyy.Flashlight;
+import me.foxyy.utils.BlockLoc;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -13,7 +14,7 @@ import java.util.*;
 
 public class UpdateLightTask extends BukkitRunnable {
 
-    static Set<Location> lightBlocks = new HashSet<>();
+    static Set<BlockLoc> lightBlocks = new HashSet<>();
 
     private static Vector getPerpendicularVector(Vector v) {
         Vector ret = new Vector(v.getZ(), v.getZ(), -v.getX()-v.getY());
@@ -23,9 +24,9 @@ public class UpdateLightTask extends BukkitRunnable {
     }
 
     public static void clear() {
-        for (Location location : lightBlocks) {
-            if (location.getBlock().getType() == Material.LIGHT) {
-                location.getBlock().setType(Material.AIR, false);
+        for (BlockLoc blockLoc : lightBlocks) {
+            if (blockLoc.getBlock().getType() == Material.LIGHT) {
+                blockLoc.getBlock().setType(Material.AIR, false);
             }
         }
     }
@@ -40,15 +41,23 @@ public class UpdateLightTask extends BukkitRunnable {
         final int phiSamples = 10;
         final int thetaSamples = 36;
 
-        final double targetPhi = configDegree * Math.PI / 180;
+
+        final double maxPhi = configDegree * Math.PI / 180;
+        final double minPhi = 10 * Math.PI / 180;
         final int targetDepth = configDepth;
 
-        Map<Location, Integer> currentLightBlocks = new HashMap<>();
-        Set<Location> lookingLocations = new HashSet<>();
+        Map<BlockLoc, Integer> currentLightBlocks = new HashMap<>();
+        Set<Material> transparentMaterialSet = new HashSet<>();
+        transparentMaterialSet.add(Material.LIGHT);
+        transparentMaterialSet.add(Material.AIR);
 
         for (Player player : Flashlight.getInstance().getServer().getOnlinePlayers()) {
             if (!Flashlight.getInstance().flashlightState.get(player))
                 continue;
+
+            List<Block> blocks = player.getLastTwoTargetBlocks(transparentMaterialSet, targetDepth);
+            BlockLoc lookingBlockLoc = new BlockLoc(blocks.getFirst().getLocation());
+            Flashlight.getInstance().getLogger().info(lookingBlockLoc.getBlock().toString());
 
             final Vector u = player.getLocation().getDirection().normalize(); // player look vector
             final Vector v = getPerpendicularVector(u); // arbitrary perpendicular vector to player look vector
@@ -56,43 +65,37 @@ public class UpdateLightTask extends BukkitRunnable {
             for (int thetaStep = 0; thetaStep < thetaSamples; thetaStep++) {
                 final double theta = 2 * Math.PI * thetaStep / thetaSamples;
                 for (int phiStep = 0; phiStep < phiSamples; phiStep++) {
-                    final double phi = targetPhi * phiStep / phiSamples;
-                    for (int depth = 0; depth <= targetDepth; depth++) {
+                    final double phi = (maxPhi - minPhi) * phiStep / phiSamples + minPhi;
+                    for (int depth = 2; depth <= targetDepth; depth++) {
                         final Vector ray = w.clone().multiply(Math.sin(phi) * Math.cos(theta))
                                 .add(v.clone().multiply(Math.sin(phi) * Math.sin(theta)))
                                 .add(u.clone().multiply(Math.cos(phi))).normalize().multiply(depth);
                         final Location location = player.getEyeLocation().clone().add(ray);
                         if (location.getBlock().getType() == Material.LIGHT
                         ||  location.getBlock().getType() == Material.AIR) {
-                            currentLightBlocks.put(location, configBrightness);
+                            BlockLoc blockLoc = new BlockLoc(location);
+                            if (!blockLoc.equals(lookingBlockLoc))
+                                currentLightBlocks.put(blockLoc, configBrightness);
                         } else if (!location.getBlock().getType().isTransparent()) {
                             break;
                         }
                     }
                 }
             }
-
-            // bugfix: cant lit fire where a light block is present
-            List<Block> lastTwoTargetBlocks = player.getLastTwoTargetBlocks(null, targetDepth);
-            if (lastTwoTargetBlocks.size() == 2 && lastTwoTargetBlocks.get(1).getType().isOccluding()) {
-                Block adjacentBlock = lastTwoTargetBlocks.getFirst();
-                lookingLocations.add(adjacentBlock.getLocation());
-            }
         }
 
-        for (Location location : lookingLocations) {
-            currentLightBlocks.remove(location);
-        }
         lightBlocks.removeAll(currentLightBlocks.keySet());
-        for (Location location : lightBlocks) {
-            if (location.getBlock().getType() == Material.LIGHT) {
-                location.getBlock().setType(Material.AIR, false);
+        for (BlockLoc blockLoc : lightBlocks) {
+            if (blockLoc.getBlock().getType() == Material.LIGHT) {
+                blockLoc.getBlock().setType(Material.AIR, false);
             }
         }
 
-        for (HashMap.Entry<Location, Integer> pair : currentLightBlocks.entrySet()) {
-            Location location = pair.getKey();
-            Block block = location.getBlock();
+//        Flashlight.getInstance().getLogger().info("Current: " + currentLightBlocks.toString());
+//        Flashlight.getInstance().getLogger().info("Remove:  " + lightBlocks.toString());
+
+        for (HashMap.Entry<BlockLoc, Integer> pair : currentLightBlocks.entrySet()) {
+            Block block = pair.getKey().getBlock();
             int lightLevel = pair.getValue();
             block.setType(Material.LIGHT, false);
             final Levelled level = (Levelled) block.getBlockData();
